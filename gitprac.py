6d1143593952ad7bfd5d2463af09646b9c058b9e
Mason Buffum <masonbuffum@outlook.com>
2025-03-11 13:39:02 -0600

    Create a wrapper function to avoid repitition

--- gitprac.py
+++ gitprac.py
@@ -19,12 +19,16 @@ def check_reboot():
     """Returns True if the computer has pending reboot."""
     return os.path.exists("/run/reboot-required")
 
+def check_root_full():
+    """Returns True if the root partition is full, False Otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def main(): 
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if check_disk_full(disk="/", min_gb=2, min_percent=10):
-        print("Disk full.")
+    if check_root_full():
+        print("Root Partition Full.")
         sys.exit(1)
     
     print("Everything ok")
